6deaab382a5ca47c3d4d44bdc3c92c1a32fc1506
gd <11874515+gregor-dietrich@users.noreply.github.com>
2022-04-24 17:30:14 +0200

    refactors

--- src/ID3/ID3.java
+++ src/ID3/ID3.java
@@ -1,17 +1,59 @@
 package ID3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public final class ID3 {
-    public static void main(String[] args) {
-        var tree = new DecisionTree();
+    public static void main(String[] args) throws FileNotFoundException {
+        final var dt = new DataTable(loadCSV("data.csv"));
 
-        var csv = new CSVReader("data.csv");
+        System.out.println(dt.calcGain("Outlook"));
+        System.out.println(dt.calcGain("Temperature"));
+        System.out.println(dt.calcGain("Humidity"));
+        System.out.println(dt.calcGain("Wind"));
+        System.out.println(dt.findMaxGainAttribute());
+        System.out.println(dt.calcMaxGain());
 
-        System.out.println(csv.calcGain("Outlook"));
-        System.out.println(csv.calcGain("Temperature"));
-        System.out.println(csv.calcGain("Humidity"));
-        System.out.println(csv.calcGain("Wind"));
+        final var tree = new DecisionTree();
+    }
 
-        System.out.println(csv.findMaxGainAttribute());
-        System.out.println(csv.calcMaxGain());
+    private static List<ArrayList<String>> loadCSV(final String filePath) throws FileNotFoundException {
+        var data = new ArrayList<ArrayList<String>>();
+        var scan = new Scanner(new File(filePath));
+        while (scan.hasNextLine()) {
+            var line = scan.nextLine();
+            var lineArrayList = new ArrayList<>(Arrays.asList(line.split(";")));
+            data.add(lineArrayList);
+        }
+        return data;
+    }
+
+    public static void printArrayList(final List<ArrayList<String>> data) {
+        var col = 0;
+        final var maxCols = new ArrayList<Integer>();
+        for (final var row : data) {
+            for (final var item : row) {
+                if (maxCols.size() <= col)
+                    maxCols.add(item.length());
+                else if (item.length() > maxCols.get(col))
+                    maxCols.set(col, item.length());
+                col++;
+            }
+            col = 0;
+        }
+
+        for (final var row : data) {
+            for (final var item : row) {
+                final var format = "| %-" + maxCols.get(col) + "s";
+                System.out.printf(format, item);
+                col++;
+            }
+            col = 0;
+            System.out.println();
+        }
     }
 }
--- src/ID3/Node.java
+++ src/ID3/Node.java
@@ -3,30 +3,21 @@ package ID3;
 import java.util.HashMap;
 
 public final class Node {
+    private final HashMap<String, Node> children = new HashMap<>();
+    private String data;
+    private Node parent;
+    private NodeType type;
+
+    public Node(final String data) {
+        setData(data);
+    }
+
     public NodeType getType() {
         return type;
     }
 
-    public void setType(NodeType type) {
+    public void setType(final NodeType type) {
         this.type = type;
-    }
-
-    public enum NodeType {
-        ROOTNODE,
-        LEAFNODE,
-        BRANCH
-    }
-    private String data;
-    private Node parent;
-    private NodeType type;
-    private final HashMap<String, Node> children = new HashMap<>();
-
-    public Node() {
-        this("");
-    }
-
-    public Node(final String data) {
-        setData(data);
     }
 
     public String getData() {
@@ -49,8 +40,8 @@ public final class Node {
         return children;
     }
 
-    public void addChild(final String name, final Node node) {
-        children.put(name, node);
+    public void addChild(final String name) {
+        children.put(name, new Node(name));
     }
 
     public void removeChild(final String name) {
@@ -69,12 +60,18 @@ public final class Node {
         this.type = type;
     }
 
-    public void print(final String prefix, boolean isTail) {
+    public void print(final String prefix, final boolean isTail) {
         System.out.println(prefix + (isTail ? "\\--" : "|--") + data);
-        var childrenList = children.keySet().stream().toList();
+        final var childrenList = children.keySet().stream().toList();
         for (int i = 0; i < childrenList.size() - 1; i++)
             children.get(childrenList.get(i)).print(prefix + (isTail ? "  " : "| "), false);
         if (children.size() > 0)
             children.get(childrenList.get(childrenList.size() - 1)).print(prefix + (isTail ? "  " : "| "), true);
+    }
+
+    public enum NodeType {
+        ROOTNODE,
+        LEAFNODE,
+        BRANCH
     }
 }
